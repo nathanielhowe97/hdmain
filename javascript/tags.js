ade2f55c9fc02c24fb272ca84eb931b9c6b32923
nathanielhowe97 <130931122+nathanielhowe97@users.noreply.github.com>
2023-07-15 15:19:29 -0500

    Add files via upload

--- javascript/tags.js
+++ javascript/tags.js
@@ -1,13 +1,14 @@
-let allTags = [
-    "product",
-    "sale",
-    "general",
-    "holiday",
-    "returning",
-    "store"
+const categoryTags = [
+    "product",     //0
+    "sale",        //1
+    "general",     //2
+    "holiday",     //3
+    "returning",   //4
+    "in progress", //5
+    "store"        //6
 ]
 
-let shopTags = [
+const shopTags = [
     "sale",
     "new",
     "50% off",
@@ -17,7 +18,7 @@ let shopTags = [
     "out of stock"
 ]
 
-let priceTags = [
+const priceTags = [
     "$24.99",
     "$29.99",
     "$49.99",
